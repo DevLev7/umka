5e0e233a54d6b56ce71003b014cff494abc39981
Vanechek1999 <vanya.kulai@yandex.ru>
2022-04-19 11:03:24 +0300

    add more air

---
+++ src/js/build-in/chooseConfigure.js
@@ -0,0 +1,8 @@
+export default function ChooseConfigure() {
+    const equipment          = document.querySelector(''),
+          additionalOptions  = document.querySelector(''),
+          chosenEquipment    = document.querySelectorAll('.configure__chosen-equipment-wrapper'),
+          counterOfEquipment = document.querySelector('.configure__chosen-equipment-counter')
+          
+
+}
