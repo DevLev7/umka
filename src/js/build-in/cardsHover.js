6abd76b6759c4b2d0e53c44e85b35427c76e31a7
Vanechek1999 <vanya.kulai@yandex.ru>
2022-06-16 15:12:35 +0300

    fix another bugs

--- src/js/build-in/cardsHover.js
+++ src/js/build-in/cardsHover.js
@@ -2,8 +2,9 @@ export default function moreButon() {
     let card = document.querySelectorAll('.all__cards__item');
     let point = document.querySelector('.svg-map');
     let test = document.querySelector('.all__container');
-
-    if(card.length > 1) {
+    
+// Так как почти на всех страницах есть all__cards__item делаем проверку, чтобы данный скрипт не ломал весь js
+    if(card.length > 1 && window.location.href.indexOf('services') == -1) {
         for (let i = 0; i < point.children.length; i++) {
             point.children[i].addEventListener('mouseenter', function (event) {
                 getImage();
